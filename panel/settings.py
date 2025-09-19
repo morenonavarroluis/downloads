357e1cd15965fa0c520e8e70de7cdadac58dfe86
Luis Navarro <lm395746@gmail.com>
2025-09-19 11:22:19 -0400

    add change in views

--- panel/settings.py
+++ panel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^s#!y!06lw0idf!g)y4x)hrmd3d+)gm@=xv5e)n&edx)ul+^(j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['manuel.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
